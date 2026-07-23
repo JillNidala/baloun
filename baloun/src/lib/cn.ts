7200192a7ffea4144c9efe3f1676715acb19cbf5
JillNidala <132025759+JillNidala@users.noreply.github.com>
2026-07-23 10:42:31 +0200

    Delete baloun directory

--- baloun/src/lib/cn.ts
+++
@@ -1,4 +0,0 @@
-// Piccola utility per unire classi condizionali senza dipendenze extra.
-export function cn(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(' ')
-}
